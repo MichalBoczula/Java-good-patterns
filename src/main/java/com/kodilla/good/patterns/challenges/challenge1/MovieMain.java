c3f4425ca0bc54c80d5486d40f5306e932da6a2c
MichalBoczula <michal.boczula0101@gmail.com>
2018-07-10 21:45:56 +0200

    up task1

--- src/main/java/com/kodilla/good/patterns/challenges/challenge1/MovieMain.java
+++ src/main/java/com/kodilla/good/patterns/challenges/challenge1/MovieMain.java
@@ -1,12 +1,16 @@
 package com.kodilla.good.patterns.challenges.challenge1;
 
+import java.util.stream.Collectors;
+
 public class MovieMain {
 
     public static void main(String[] args){
         final MovieStore movieStore = new MovieStore();
 
-        movieStore.getMovies().entrySet().stream()
+        String result = movieStore.getMovies().entrySet().stream()
                 .map(entry -> entry.getKey()+": "+entry.getValue())
-                .forEach(e -> System.out.print(e+" ! "));
+                .collect(Collectors.joining("!"));
+
+        System.out.println(result);
     }
 }
