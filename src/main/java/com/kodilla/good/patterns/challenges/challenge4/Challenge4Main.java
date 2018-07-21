6abbf888865c1e5dcc40f1236750e5bafe0797c3
MichalBoczula <michal.boczula0101@gmail.com>
2018-07-21 18:04:01 +0200

    task 4 corrected and cleaned

--- src/main/java/com/kodilla/good/patterns/challenges/challenge4/Challenge4Main.java
+++ src/main/java/com/kodilla/good/patterns/challenges/challenge4/Challenge4Main.java
@@ -4,12 +4,6 @@ public class Challenge4Main {
 
     public static void main(String[] args){
         final FlightService flightService = new FlightService();
-        flightService.findAllFlightFrom("berlin").stream()
-                .forEach(System.out::println);
-        flightService.findFlightToWithoutCharge("warsaw").stream()
-                .forEach(System.out::println);
-        flightService.findFlightToWithCharge("warsaw", "london").stream()
-                .forEach(System.out::println);
-        System.out.println(flightService.isFlightAvalieble("warsaw", "london"));
+        System.out.println(flightService.isFlightAvailable("warsaw", "london"));
     }
 }
